0b5e395b3c29686106f00626fc56d68cb2e1c0ee
mgw1992 <meagan.wolf18@gmail.com>
2016-04-26 19:00:41 -0400

    Updates Parts 1 and 2 of Milestone 1

--- app/src/main/java/com/example/dad/mystreamer/MainActivity.java
+++ app/src/main/java/com/example/dad/mystreamer/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.dad.mystreamer;
 
+
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.SearchView;
+import kaaes.spotify.webapi.samplesearch.SearchResultsAdapter;
+import kaaes.spotify.webapi.samplesearch.ResultListScrollListener;
+import kaaes.spotify.webapi.samplesearch.SearchPresenter;
+import kaaes.spotify.webapi.android.models.Track;
+
+import java.util.List;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Search.View {
 
-    private static final String TAG = "ArtistResult" ;
+    private static final String TAG = "ArtistResult";
     private ImageView image1;
+
+    static final String EXTRA_TOKEN = "EXTRA_TOKEN";
+    private static final String KEY_CURRENT_QUERY = "CURRENT_QUERY";
+
+    private Search.ActionListener mActionListener;
+
+    private LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+    private ScrollListener mScrollListener = new ScrollListener(mLayoutManager);
+    private SearchResultsAdapter mAdapter;
+
+    private class ScrollListener extends ResultListScrollListener {
+
+        public ScrollListener(LinearLayoutManager layoutManager) {
+            super(layoutManager);
+        }
+
+        @Override
+        public void onLoadMore() {
+            mActionListener.loadMoreResults();
+        }
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +57,85 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
-        image1 = (ImageView) findViewById (R.id.image1);
+        image1 = (ImageView) findViewById(R.id.image1);
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(image1);
-        
+
+        Intent intent = getIntent();
+        String token = intent.getStringExtra(EXTRA_TOKEN);
+
+        mActionListener = new SearchPresenter(this, this);
+        mActionListener.init(token);
+
+        final SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mActionListener.search(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        mAdapter = new SearchResultsAdapter(this, new SearchResultsAdapter.ItemSelectedListener() {
+            @Override
+            public void onItemSelected(View itemView, Track item) {
+                mActionListener.selectTrack(item);
+            }
+        });
+
+        // RecyclerView resultsList = (RecyclerView) findViewById(R.id.search_results);
+        //resultsList.setHasFixedSize(true);
+        //resultsList.setLayoutManager(mLayoutManager);
+        //resultsList.setAdapter(mAdapter);
+        //resultsList.addOnScrollListener(mScrollListener);
+
+        if (savedInstanceState != null) {
+            String currentQuery = savedInstanceState.getString(KEY_CURRENT_QUERY);
+            mActionListener.search(currentQuery);
+        }
     }
 
 
+    @Override
+    public void reset() {
+        mScrollListener.reset();
+        mAdapter.clearData();
+    }
+
+    @Override
+    public void addData(List<Track> items) {
+        mAdapter.addData(items);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mActionListener.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mActionListener.resume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mActionListener.getCurrentQuery() != null) {
+            outState.putString(KEY_CURRENT_QUERY, mActionListener.getCurrentQuery());
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mActionListener.destroy();
+        super.onDestroy();
+    }
 }

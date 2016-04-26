786c56a610af092d4196bdf7a710d8c45a94e4d0
mgw1992 <meagan.wolf18@gmail.com>
2016-04-26 18:01:27 -0400

    Parts 1 and 2 of Milestone 1

--- app/src/main/java/com/example/dad/mystreamer/MainActivity.java
+++ app/src/main/java/com/example/dad/mystreamer/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.dad.mystreamer;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "ArtistResult" ;
     private ImageView image1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
         image1 = (ImageView) findViewById (R.id.image1);
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(image1);
+        
     }
+
+
 }
